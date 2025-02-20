c894109a81b7146278db762c077ef0c02757571a
Myo Thant <myothant.thedev@gmail.com>
2025-02-21 02:26:10 +0700

    feat(models): update User model with additional fields and signals)

--- server/models/user.py
+++ server/models/user.py
@@ -1,11 +1,31 @@
 from mongoengine.fields import *
-from mongoengine import Document, signals
+from mongoengine import Document, EmbeddedDocument,signals
 import bcrypt
+from flask_login import UserMixin
 
-class User(Document):
+role_types = {
+  ('ADMIN', 'admin'),
+  ('MODIRATOR', 'modirator'),
+  ('USER', 'user')
+}
+
+permission_types = (
+  ('R', 'read'),
+  ('W', 'write'),
+  ('RW', 'readWrite'),
+  ('RWD', 'readWriteDelete')
+)
+
+class Role(EmbeddedDocument):
+  role = StringField(max_length=10, min_length=4, choices=role_types)
+  permission = StringField(max_length=3, choices=permission_types)
+
+class User(Document, UserMixin):
   username = StringField(max_length=35, required=True)
   password = StringField(required=True, min_length=8)
   email = EmailField(required=True, unique=True)
+  image = StringField()
+  role = EmbeddedDocumentField(Role)
 
   meta = { 'collection': 'users'}
 
@@ -16,5 +36,12 @@ class User(Document):
       salt = bcrypt.gensalt()
       hashed = bcrypt.hashpw(document.password.encode('utf-8'), salt)
       document.password = hashed.decode('utf-8')
+
+  def to_dict(self):
+    return {
+      "id": str(self.id),
+      "username": self.username,
+      "email" : self.email
+    }
 
 signals.pre_save.connect(User.pre_save, sender=User)
