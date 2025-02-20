3fb4d8cd22cd117ea54716b95c4baec69398ab22
Myo Thant <myothant.thedev@gmail.com>
2025-02-21 02:30:31 +0700

    feat(services): add User service for user related logic

---
+++ server/services/user_services.py
@@ -0,0 +1,10 @@
+from server.models.user import User
+
+# user profile
+def user_detail_service():...
+
+# user update profile
+def user_update_service():...
+
+# user regiester
+def account_register():...
