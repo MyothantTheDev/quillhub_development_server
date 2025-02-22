2e9d322115a6b34c34fee532a09c672ca0b6f932
Myo Thant <myothant.thedev@gmail.com>
2025-02-23 02:52:49 +0700

    feat(model): add reset token feature in User model

--- server/models/user.py
+++ server/models/user.py
@@ -2,6 +2,8 @@ from mongoengine.fields import *
 from mongoengine import Document, EmbeddedDocument,signals
 import bcrypt
 from flask_login import UserMixin
+from itsdangerous import TimedSerializer as Serializer
+from flask import current_app
 
 role_types = {
   ('ADMIN', 'admin'),
@@ -31,17 +33,21 @@ class User(Document, UserMixin):
 
   @classmethod
   def pre_save(cls, sender, document, **kwargs):
-    # check if the password is already hashed.
-    if not document.password.startwith("$2"):
-      salt = bcrypt.gensalt()
-      hashed = bcrypt.hashpw(document.password.encode('utf-8'), salt)
-      document.password = hashed.decode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(document.password.encode('utf-8'), salt)
+    document.password = hashed.decode('utf-8')
 
   def to_dict(self):
     return {
       "id": str(self.id),
       "username": self.username,
-      "email" : self.email
+      "email" : self.email,
+      "image": self.image,
+      "role" : self.role
     }
+  
+  def get_reset_token(self, expire_sec=1800):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    return s.dump({'user_id': self.id}).decode('utf-8')
 
 signals.pre_save.connect(User.pre_save, sender=User)
