7cd4e1bd8c2ac6694cc8e5e7ee2c67b5309c91b3
Myo Thant <myothant.thedev@gmail.com>
2025-02-24 21:08:03 +0700

    feat(routes): add logout route

--- server/routes/user_route.py
+++ server/routes/user_route.py
@@ -8,3 +8,4 @@ users_bp.add_url_rule('/account', methods=['GET'], view_func=users.user_detail_s
 users_bp.add_url_rule('/account/update', methods=['POST'], view_func=users.user_update_service)
 users_bp.add_url_rule('/register', methods=['POST'], view_func=users.account_register)
 users_bp.add_url_rule('/login', methods=['POST'], view_func=users.user_login)
+users_bp.add_url_rule('/logout', methods=['GET'], view_func=users.user_logout)
