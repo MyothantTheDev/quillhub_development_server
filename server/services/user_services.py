115ce62630b8a3d31cdd9c094f1ec558f0fc6390
Myo Thant <myothant.thedev@gmail.com>
2025-03-07 22:42:02 +0700

    feat(services): add user update service to update user informations

--- server/services/user_services.py
+++ server/services/user_services.py
@@ -1,11 +1,17 @@
 from server.models.user import User
 from server.models.favourite import UserFavouriteArticle
-from server.schema.user_schema import RegisterSchema, ValidationError, LoginSchema
+from server.schema.user_schema import (
+  RegisterSchema, 
+  ValidationError, 
+  LoginSchema, 
+  UpdateAccountSchema
+)
 from flask import request, jsonify, session, make_response
 from copy import deepcopy
 from server.utils.token_generator import token_generator, jwt_required
 from flask_login import login_user, current_user, logout_user, login_required
 import bcrypt
+from server.utils.image_utils import save_picture
 
 # user profile
 @jwt_required
@@ -20,7 +26,20 @@ def user_detail_service():
 # user update profile
 @jwt_required
 @login_required
-def user_update_service():...
+def user_update_service():
+  request_cp = {}
+  request_cp['username'] = request.form.get('username')
+  request_cp['image'] = request.files.get('image')
+  validated = UpdateAccountSchema().load(request_cp)
+  user = User.objects(id__exact = current_user.id).first()
+  if current_user.username != validated['username'] and validated['username'] != "":
+    user.username = validated['username']
+  if current_user.image != validated['image'] and validated['image'] != None:
+    img_path = save_picture(validated['image'])
+    user.image = img_path
+  if current_user.username != user.username or current_user.image != user.image:
+    user.save()
+  return {'status': 200, 'message': 'Success Update.', 'data': user.to_dict()}
 
 # user regiester
 def account_register():
@@ -61,7 +80,7 @@ def user_login():
       login_user(user, remember=validated_result.get('remember'))
       jwtToken = token_generator(user)
       session['jwtToken'] = jwtToken
-      response = make_response({'status': 200, 'message': 'Login Successful.'})
+      response = make_response({'status': 200, 'message': 'Login Successful.', 'data': user.to_dict()})
       response.set_cookie('token', jwtToken, httponly=True)
       return response
     return {'status': 401, 'message': 'Email or password is wrong or both!'}
