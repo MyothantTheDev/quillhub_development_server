12b3ecbc5d716c7b2f725a2c06bd091f46e9cd49
Myo Thant <myothant.thedev@gmail.com>
2025-03-07 22:35:35 +0700

    chore(git): update .gitignore to exclude addtional files

--- server/schema/user_schema.py
+++ server/schema/user_schema.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, validate, validates, ValidationError, fields, post_load, validates_schema
 import re
+import os
 
 ROLE_TYPES = ['ADMIN', 'MODIRATOR', 'USER']
 PERMISSION_TYPES = ['R','W','RW','RWD']
@@ -51,7 +52,7 @@ class RegisterSchema(Schema):
   
 
 class LoginSchema(Schema):
-  email = fields.Email()
+  email = fields.Email(required=True)
   password = fields.String(required=True, load_only=True)
   remember = fields.Bool()
 
@@ -65,3 +66,24 @@ class LoginSchema(Schema):
       raise ValidationError("Password must contain at least one number.")
     if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
       raise ValidationError("Password must contain at least one special character.")
+
+class UpdateAccountSchema(Schema):
+  username = fields.String()
+  image = fields.Raw()
+
+  @validates('image')
+  def validate_image(self, value):
+    if not hasattr(value, 'filename'):
+      raise ValueError("Invalid file object.")
+    
+    allowed_extensions = {'.png', '.jpg', '.jpeg', '.gif'}
+    _, ext = os.path.splitext(value.filename)
+    if ext.lower() not in allowed_extensions:
+      raise ValidationError(f"File type '{ext}' not allowed.")
+    
+    value.seek(0, os.SEEK_END)
+    size = value.tell()
+    value.seek(0)
+    max_size = 5 * 1024 * 1024
+    if size > max_size:
+      raise ValidationError("File is too large. Maximum size is 5MB")
