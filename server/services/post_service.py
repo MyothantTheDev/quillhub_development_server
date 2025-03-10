be068e288bf5714db336d0769133f998dcacc044
Myo Thant <myothant.thedev@gmail.com>
2025-03-10 23:09:23 +0700

    feat(services): add skeleton structure for post services

--- server/services/post_service.py
+++ server/services/post_service.py
@@ -1,9 +1,20 @@
-
+from flask_login import login_required
+from server.utils.token_generator import jwt_required, adminauthorized_required
 
 def trending_articles():...
 
+@adminauthorized_required
+@jwt_required
+@login_required
 def new_articles():...
 
 def detail_article():...
 
+@adminauthorized_required
+@jwt_required
+@login_required
 def delete_article():...
+
+@jwt_required
+@login_required
+def add_comment():...
