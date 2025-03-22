383df5266f2bb6aa4954674eebff4d355c8abe38
Myo Thant <myothant.thedev@gmail.com>
2025-03-22 21:31:51 +0700

    fix(services): remove new article service and add to admin service

--- server/services/post_service.py
+++ server/services/post_service.py
@@ -1,12 +1,10 @@
-from flask_login import login_required
+from flask import request, jsonify
+from server.models.article import Article, ArticleBlock
+from flask_login import current_user, login_required
 from server.utils.token_generator import jwt_required, adminauthorized_required
+import json
 
 def trending_articles():...
-
-@adminauthorized_required
-@jwt_required
-@login_required
-def new_articles():...
 
 def detail_article():...
 
