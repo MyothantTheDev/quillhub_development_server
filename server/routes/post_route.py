aea41a882056e8ffe6a851da7f379c04d450d036
Myo Thant <myothant.thedev@gmail.com>
2025-03-22 21:22:29 +0700

    fix(routes): remove post new article and add in admin route

--- server/routes/post_route.py
+++ server/routes/post_route.py
@@ -3,9 +3,19 @@ from server.services import post_service
 
 post_bp = Blueprint('posts', __name__)
 
-post_bp.add_url_rule('/post', methods=['GET'], view_func=post_service.trending_articles)
-post_bp.add_url_rule('/post/add', methods=['POST'], view_func=post_service.new_articles)
-post_bp.add_url_rule('/post/detail', methods=['GET'], view_func=post_service.detail_article)
-post_bp.add_url_rule('/post/detail/delete', methods=['POST'], view_func=post_service.delete_article)
+# route prefix
+route_prefix = '/post/'
 
-post_bp.add_url_rule('/post/comment/add', methods=['POST'], view_func=post_service.add_comment)
+# post route contansts
+post_trending = route_prefix+'trending'
+post_detial = route_prefix+'detial'
+post_delete = post_detial+'/delete'
+post_comment = route_prefix+'comment/add'
+
+# post routes
+post_bp.add_url_rule(post_trending, methods=['GET'], view_func=post_service.trending_articles)
+post_bp.add_url_rule(post_detial, methods=['GET'], view_func=post_service.detail_article)
+post_bp.add_url_rule(post_delete, methods=['POST'], view_func=post_service.delete_article)
+
+# comment routes
+post_bp.add_url_rule(post_comment, methods=['POST'], view_func=post_service.add_comment)
