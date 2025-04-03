8cec15b7587c308361d0b62bfb1f4eb890f5a095
Myo Thant <myothant.thedev@gmail.com>
2025-04-04 01:08:07 +0700

    feat(models): add comment date as datetime field for sorting

--- server/models/comment.py
+++ server/models/comment.py
@@ -1,12 +1,14 @@
 from mongoengine.fields import *
-from mongoengine import CASCADE
+from mongoengine import CASCADE, DateTimeField
 from mongoengine import Document
 from server.models.article import Article
 from server.models.user import User
+from datetime import datetime as dt
 
 class Comment(Document):
   article = ReferenceField(Article, reverse_delete_rule=CASCADE, require=True)
   user = ReferenceField(User, reverse_delete_rule=CASCADE, require=True)
   comment = StringField()
+  comment_date = DateTimeField(default=dt.now, required=True)
 
   meta = { 'collection' : 'comments'}
