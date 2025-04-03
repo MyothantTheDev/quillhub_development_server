63db63c76b39bbbe94f22d55a1eb830422bf459b
Myo Thant <myothant.thedev@gmail.com>
2025-04-03 19:58:29 +0700

    feat(services): add all article and categories articles services

--- server/services/post_service.py
+++ server/services/post_service.py
@@ -1,10 +1,62 @@
-from flask import request, jsonify
-from server.models.article import Article, ArticleBlock
+from flask import jsonify
+from server.models.article import Article
 from flask_login import current_user, login_required
 from server.middleware.token_generator import jwt_required, adminauthorized_required
-import json
+import re
 
-def trending_articles():...
+def all_article(page_number):
+  try:
+    re_pattern = r'\b\d+\b'
+    if not re.fullmatch(re_pattern, page_number):
+      return jsonify({'status': 401, 'message': 'Page number should be integer.'}), 401
+    page_number = int(page_number)
+    page_size = 5
+    begin = (page_number - 1) * page_size
+    end = begin + page_size
+    articles = Article.objects[begin:end]()
+    articles = [article.to_json() for article in articles]
+    return jsonify(
+      {
+        'status': 200, 
+        'message': 'Retrieved articles successful.',
+        'data': articles
+      }
+      ), 200
+  except:
+    return jsonify(
+      {
+        'status': 401,
+        'message': 'Something went wrong.'
+      }
+    ), 401
+  
+
+def categories_articles(category, page_number):
+  try:
+    re_pattern = r'\b\d+\b'
+    if not re.fullmatch(re_pattern, page_number):
+      return jsonify({'status': 401, 'message': 'Page number should be integer.'}), 401
+    page_number = int(page_number)
+    page_size = 5
+    begin = (page_number - 1) * page_size
+    end = begin + page_size
+    articles = Article.objects(tags=category)[begin, end]
+    articles = [article.to_json() for article in articles]
+    return jsonify(
+      {
+        'status': 200,
+        'message': f'Retrieved {category} articles successful.',
+        'data': articles
+      }
+    ), 200
+  except:
+    return jsonify(
+      {
+        'status': 401,
+        'message': 'Something went wrong.'
+      }
+    )
+
 
 def detail_article():...
 
