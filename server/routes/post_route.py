d9c0b1f164a0329effe5a1436dc2052174d7484a
Myo Thant <myothant.thedev@gmail.com>
2025-04-03 19:53:04 +0700

    feat(routes): add post all route

--- server/routes/post_route.py
+++ server/routes/post_route.py
@@ -1,17 +1,18 @@
 from flask import Blueprint
 from server.services import post_service
 from server.routes.route_endpoints import (
-  post_trending, post_detial, post_delete,
-  post_comment
+  post_category, post_detial, post_delete,
+  post_comment, post_all
 )
 
 post_bp = Blueprint('posts', __name__)
 
 
 # post routes
-post_bp.add_url_rule(post_trending, methods=['GET'], view_func=post_service.trending_articles)
+post_bp.add_url_rule(post_category, methods=['GET'], view_func=post_service.categories_articles)
 post_bp.add_url_rule(post_detial, methods=['GET'], view_func=post_service.detail_article)
 post_bp.add_url_rule(post_delete, methods=['POST'], view_func=post_service.delete_article)
+post_bp.add_url_rule(post_all, methods=['GET'], view_func=post_service.all_article)
 
 # comment routes
 post_bp.add_url_rule(post_comment, methods=['POST'], view_func=post_service.add_comment)
