84f7969237244f433219b9785b51ff159fd85079
Myo Thant <myothant.thedev@gmail.com>
2025-04-11 06:41:24 +0700

    feat(entry): add system error handling and create main function for more security and structure

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from server import Server
+import argparse
+import sys
+import logging
+import os
+from datetime import datetime as dt
 
-server = Server()
+def handle_exception(exc_type, exc_value, exc_traceback):
+  cwd = os.path.join(os.getcwd(), 'log')
+  log_name = dt.now().isoformat()
+  if issubclass(exc_type, KeyboardInterrupt): return
+  logging.basicConfig(filename=os.path.join(cwd, f'log{log_name}.txt'), level=logging.ERROR)
+  logging.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+def main():
+  sys.excepthook = handle_exception
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-prod', '--production', type=bool)
+  args = parser.parse_args()
+  production = args.production if args.production != None else False
+  server = Server()
+  server.start(production)
 
 if __name__ == "__main__":
-  server.start()
+  main()
